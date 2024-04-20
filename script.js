14479cf304e04e811c859327a777860170199e1a
MatthewMalefyt <157759162+MatthewMalefyt@users.noreply.github.com>
2024-04-20 19:09:35 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() { //open menu to access views or close menu
-    const menu = document.querySelector(".menu-links"); 
-    const icon = document.querySelector(".hamurger-icon"); 
-    menu.classList.toggle("open"); 
-    icon.classList.toggle("open");
-}
